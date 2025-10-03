76583fc9a07055fccf20aeffc849a6577b234c96
Jessica <jpanndora@gmail.com>
2025-10-03 20:31:47 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Distancia percorrida (m)");
+            double distancia = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Tempo gasto (s)");
+            double tempo = Convert.ToDouble(Console.ReadLine());
+            double velocidade = distancia / tempo;
+            Console.WriteLine($"\n velocidade média: {velocidade:N2} m/s");
